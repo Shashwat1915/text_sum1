9007ee0b6401a069d7364d276f9004a9e13e0fff
Rio <tiwarishahswat2016@gmail.com>
2024-12-04 16:59:33 +0530

    Files

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request
 from transformers import pipeline
 from urllib.parse import quote as url_quote
+import psutil
+import os
+import gc
+
+pid = os.getpid()
+python_process = psutil.Process(pid)
+
+memory_use = python_process.memory_info().rss / 1024 ** 2
+print(f"Memory usage: {memory_use:.2f} MB")
+
 
 app = Flask(__name__)
 
-summarizer = pipeline("summarization", model="t5-small")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-6-6", device=-1)
+
 
 @app.route("/")
 def home():
@@ -17,10 +28,15 @@ def summarize():
     if not input_text or len(input_text.split()) < 30:
         summary = "The input text is too short to summarize. Please provide a longer text."
     else:
-        # Generate the summary
-        summary = summarizer(input_text, max_length=150, min_length=50, do_sample=False)[0]['summary_text']
+        # Split text into chunks of up to 500 words
+        chunks = [input_text[i:i+500] for i in range(0, len(input_text), 500)]
+        summaries = [summarizer(chunk, max_length=150, min_length=50, do_sample=False)[0]['summary_text'] for chunk in chunks]
+        summary = " ".join(summaries)
     
     return render_template("index.html", original_text=input_text, summary=summary)
 
+    del summarizer
+    gc.collect()
+    
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
